c91a3db3e14539398ab16517a891b55390e82e5c
Muhammad H. <39835006+ShubsMO6@users.noreply.github.com>
2026-04-26 22:26:39 -0400

    vec3 class finished

--- src/vec3.h
+++ src/vec3.h
@@ -3,6 +3,8 @@
 
 #include <cmath>
 #include <iostream>
+#include <ostream>
+#include <istream>
 
 class vec3 {
     public:
@@ -50,4 +52,49 @@ class vec3 {
 };
 
 using point3 = vec3;
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& x, const vec3& y) {
+    return vec3(x.e[0] + y.e[0], x.e[1] + y.e[1], x.e[2] + y.e[2]);
+}
+
+inline vec3 operator-(const vec3& x, const vec3& y) {
+    return vec3(x.e[0] - y.e[0], x.e[1] - y.e[1], x.e[2] - y.e[2]);
+
+}
+
+inline vec3 operator*(const vec3& x, const vec3& y) {
+    return vec3(x.e[0] * y.e[0], x.e[1] * y.e[1], x.e[2] * y.e[2]);
+}
+
+inline vec3 operator*(const vec3& x, double y) {
+    return vec3(x.e[0] * y, x.e[1] * y, x.e[2] * y);
+}
+
+inline vec3 operator*(double y, const vec3& x) {
+    return vec3(x * y);
+}
+
+inline vec3 operator/(const vec3& x, double y) {
+    return vec3(x * (1/y));
+}
+
+inline double dot(const vec3& a, const vec3& b) {
+    return a.e[0] * b.e[0]
+        + a.e[1] * b.e[1]
+        + a.e[2] * b.e[2];
+}
+
+inline vec3 cross(const vec3& a, const vec3& b) {
+    return vec3((a.e[1] * b.e[2] - b.e[1] * a.e[2]), 
+            -(a.e[0] * b.e[2] - a.e[2] * b.e[0]),
+            (a.e[0] * b.e[1] - b.e[0] * a.e[1]));
+}
+
+inline vec3 unit_vector(const vec3& a) {
+    return a / a.length();
+}
 
